f50c4224b3da65db1528bc306177d710a659ace8
Haliaas <mahalia.asanaenyi@gmail.com>
2011-05-17 22:29:38 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampApp::Application.config.secret_token = '67d781f97e342e2beab491c4833752530897e44470b475ab115621bbe5e3d06390729813fd2c0b0e66cb31d3a02bc40fa9fc6ccfb4739e4aec54cb6bc04ed816'
